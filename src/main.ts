2473c12bf055637908efdb5039b2bf8210036ee3
Peyman <Peymanc@hotmail.com>
2018-09-02 01:04:34 -0400

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+//this page in angular app gets executed first
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -7,6 +8,10 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//this is really the code that starts the application.
+//abvove is importing certain packages, like require in nodejs. 
+//below you are providing the AppModule, the name of your module sending it as an argument 
+//the app.module.
+//angular then parses this module with those components inside app.module.ts
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
